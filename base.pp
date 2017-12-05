a96054ca848f96a2c34bf5f5c2f24161afc92643
Mario Adrián Domínguez <adrian@eiris.es>
2017-12-05 14:35:19 +0100

    Primer manifiesto

---
+++ base.pp
@@ -0,0 +1,29 @@
+user { 'dojo':
+    ensure      => absent,
+}
+
+exec { 'apt-get update':
+    command     => '/usr/bin/apt-get update',
+}
+
+package { [ 'tree', 'wget', 'git', 'unzip', 'ntp' ]: 
+    ensure      => present,
+}
+
+file { '/etc/motd':
+    ensure      => present,
+    owner       => 'root',
+    group       => 'root',
+    mode        => '0644',
+    path        => '/etc/motd',
+    content     => '
+    
+        Property of XYZ
+    
+    ',
+}
+
+service { 'ntp':
+    ensure      => running,
+    enable      => true,
+}
